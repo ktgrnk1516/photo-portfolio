652f609645c9461edd1953dfc4d57cf95a9bdde8
ktgrnk1516 <katagirio1516@gmail.com>
2022-03-25 00:04:28 +0900

    時間によって画像の並び順を変える

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { Header, PostCard } from "../components";
 import { getPosts } from "../services";
 import { useEffect, useState } from "react";
 
-export default function Home({ isVisible, posts }) {
+export default function Home({ posts }) {
   //ローカルのpostデータ=>graphCMSから持ってくるからコメントアウト
   // const posts = [
   //   {
@@ -16,70 +16,73 @@ export default function Home({ isVisible, posts }) {
   //   },
   // ];
 
-  // //スクロールによって背景色を変える
-  // const [isVisible, setIsVisible] = useState(false);
-
-  // const toggleVisibility = () => {
-  //   window.scrollY > 100 ? setIsVisible(true) : setIsVisible(false);
-  // };
-
-  // useEffect(() => {
-  //   window.addEventListener("scroll", toggleVisibility);
-  //   return () => window.removeEventListener("scroll", toggleVisibility);
-  // }, []);
-
-  //postsをmapする？
-  // const getParseTime = () => {
   useEffect(() => {
-    let times = posts.map((post) => {
+    //配列postsをソート/オブジェクトの昇順ソート
+    //https://keizokuma.com/js-array-object-sort/
+    //post.node.timeを時間のみの形にしたい（mapで？）
+    let posttimes1 = posts.map((post) => {
       return post.node.time; //整形したい
     });
-    const posttimes = times.map((time) => {
+    //AM PMにしないverでいけそう
+    //mapでUTC時間をAM/PMの形に整形
+    const sorttimes = posttimes1.map((time) => {
       const a = Date.parse(time);
       const b = new Date(a);
 
-      // console.log(time);
-      // console.log(a);
-      // console.log(b);
-
-      const hh = b.getHours().toString();
+      //https://qiita.com/Naoki_kkc/items/2a29287834c453d23ecf
+      //JavaScriptで時刻の二桁目をゼロ埋めするQiita
+      //.toString().padStart(2, '0')!!!!!!
+      const hh = b.getHours().toString().padStart(2, "0");
       const mm = b.getMinutes().toString().padStart(2, "0");
-      const saisyutimes = hh < 12 ? `AM ${hh}:${mm}` : `PM ${hh - 12}:${mm}`;
+      const saisyutimes = `${hh}:${mm}`;
 
-      return saisyutimes.toString(); //mapでreturn!!!
+      return saisyutimes; //mapでreturn!!!
+    });
+    // sorttimes.sort(function (a, b) {
+    //   return a > b ? 1 : -1;
+    // });
+    // console.log(sorttimes);
+
+    //配列を書き換え
+    posts.map((post, index) => {
+      post.node.time = sorttimes[index];
     });
 
-    console.log(posttimes);
+    console.log([posts]);
 
-    // const chosa = posts.map((post) => {
-    //   return post.node.time;
+    //配列posts：オブジェクトの時間の昇順ソート
+    let result = posts.sort(function (a, b) {
+      return a.node.time < b.node.time ? -1 : 1;
+    });
+
+    console.log([result]);
+
+    //-----------------------------------//
+
+    //AM/PMの形に整形
+
+    let times = result.map((post) => {
+      return post.node.time; //整形したい
+    });
+
+    //mapでUTC時間をAM/PMの形に整形
+    const posttimes2 = times.map((time) => {
+      console.log(time.substr(0, 2));
+      const HH = time.substr(0, 2);
+      const MM = time.substr(3, 2);
+
+      const saisyutimes =
+        HH < 12 ? `AM ${parseInt(HH)}:${MM}` : `PM ${HH - 12}:${MM}`;
+
+      return saisyutimes; //mapでreturn!!!
+    });
+
+    console.log(posttimes2);
 
     posts.map((post, index) => {
-      post.node.time = posttimes[index];
+      post.node.time = posttimes2[index];
     });
   }, [posts]);
-
-  // console.log(chosa[0]);
-  // chosa[0] = posttimes[0];
-
-  // chosa = posttimes
-  // console.log(chosa);
-  //----------------------------------//
-  // for ですべての要素に行いたい。
-  // for (let i = 0; i < times.length; i++) {
-  //   const a = Date.parse(times[i]);
-  //   const b = new Date(a);
-  //   const hh = b.getHours();
-  //   const mm = b.getMinutes().toString().padStart(2, "0");
-
-  //   const posttimes = hh < 12 ? `AM ${hh}:${mm}` : `PM ${hh - 12}:${mm}`;
-  //   console.log(posttimes);
-
-  //https://qiita.com/Naoki_kkc/items/2a29287834c453d23ecf
-  //JavaScriptで時刻の二桁目をゼロ埋めするQiita
-  //.toString().padStart(2, '0')!!!!!!
-  // };
-  // getParseTime();
 
   return (
     <div>
